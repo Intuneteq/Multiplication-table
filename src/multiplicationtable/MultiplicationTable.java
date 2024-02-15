3c11fc657c4e1c0c271deda75ed30a57eb474f1c
Intuneteq <tobiolanitori@gmail.com>
2024-02-15 00:47:02 +0000

    update

--- src/multiplicationtable/MultiplicationTable.java
+++ src/multiplicationtable/MultiplicationTable.java
@@ -1,4 +1,3 @@
-
 package multiplicationtable;
 
 import java.util.Scanner;
@@ -9,17 +8,16 @@ import java.util.Scanner;
  */
 public class MultiplicationTable {
 
-    
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int table;
-        
+
         System.out.println("What multiplication table do you want to display? ");
         table = console.nextInt();
-        
-        for(int i = 1; i <=12; i++) {
-            System.out.println(table + " x " + i + " = " + (table*i));
+
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(table + " x " + i + " = " + (table * i));
         }
     }
-    
+
 }
